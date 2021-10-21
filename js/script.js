66ebd4feb9e10ef90b871d4f0c413bd47b19a952
Suzanna Mitchell <suziequ33@gmail.com>
2021-10-21 12:41:19 -0500

    editing comments and adding more quotes

--- js/script.js
+++ js/script.js
@@ -18,7 +18,6 @@ const colors =[
 ];
 
 /** I used the same formula of the random number method for the colors.
- * 
  */
 function getRandomColor(){
   let randomColor = Math.floor(Math.random() * colors.length);
@@ -30,21 +29,25 @@ function getRandomColor(){
  * `quotes` array 
 ***/
 /**  array of objects quotes: Harry Pottr quotes with a source, citation, and chapter. 
- **/
+ * read the directions for the project... they help....
+
+**/
 
 const quotes =[
   {quote:"It does not do to dwell on dreams and forget to live.", source:"-Albus Dumbledore", citation:"Harry Potter and the Sorceret's Stone", chapter:"Chapter 12"},
-  {quote:"The ones that love us never really leave us. You can always find them in here.", source:"-Sirius Black", citation:"Harry Potter and the Prisoner of Azkaban."},
-  {quote:"Things we lose have a way of coming back to us in the end, if not always in the way we expect.", source: "-Luna Lovegood", citation:"Harry Potter and the Order of the Phoenix."},
-  {quote:"Dobby had heard of your greatness, sir, but of your goodness, Dobby never knew.",source:"-Dobby", citation:"Harry Potter and the Chamber of Secrets."},
-  {quote:"Do not pity the dead, Harry. Pity the living . And above all, pity those who live without love.", source:"-Albus Dumbledore", citation:"Harry Potter and Deathly Hallows."},
-  {quote:"We've all got both light and dark inside us. What matters is the part we choose to act on...that's who we really are.", source:"-Sirius Black",citation:"Harry Potter and the Order of the Phoenix."},
-  {quote:"When in doubt, go to the library.", source:"-Ron Weasley",citation:"Harry Potter and the Chamber of the Secrets.", chapter:"Chapter 14"},
-  {quote:"Don't let the muggles get you down.", source:"-Ron Weasley",citation:"Harry Potter and the Prisoner of Azkaban.", chapter: "Chapter 1"},
-  {quote:"Until the very end.", source:"-James Potter", citation:"Harry Potter and the Deathly Hallows."},
+  {quote:"The ones that love us never really leave us. You can always find them in here.", source:"-Sirius Black", citation:"Harry Potter and the Prisoner of Azkaban"},
+  {quote:"Things we lose have a way of coming back to us in the end, if not always in the way we expect.", source: "-Luna Lovegood", citation:"Harry Potter and the Order of the Phoenix"},
+  {quote:"Dobby had heard of your greatness, sir, but of your goodness, Dobby never knew.",source:"-Dobby", citation:"Harry Potter and the Chamber of Secrets"},
+  {quote:"Do not pity the dead, Harry. Pity the living . And above all, pity those who live without love.", source:"-Albus Dumbledore", citation:"Harry Potter and Deathly Hallows"},
+  {quote:"We've all got both light and dark inside us. What matters is the part we choose to act on...that's who we really are.", source:"-Sirius Black",citation:"Harry Potter and the Order of the Phoenix"},
+  {quote:"When in doubt, go to the library.", source:"-Ron Weasley",citation:"Harry Potter and the Chamber of the Secrets", chapter:"Chapter 14"},
+  {quote:"Don't let the muggles get you down.", source:"-Ron Weasley",citation:"Harry Potter and the Prisoner of Azkaban", chapter: "Chapter 1"},
+  {quote:"Until the very end.", source:"-James Potter", citation:"Harry Potter and the Deathly Hallows"},
   {quote:"Just because you have the emotional range of a teaspoon doesn't mean we all have.", source:"-Hermione Granger", citation:"Harry Potter and the Order of the Phoenix", chapter:"Chapter 21"},
-  {quote:"You sort of start thinking anything's possible if you've got enough nerve.",source:"-Ginny Weasley", citation:"Harry Potter and the Order of the Phoenix."},
-  {quote:"I love magic.", source:"-Harry Potter", citation:" Harry Potter and the Goblet of Fire."},
+  {quote:"You sort of start thinking anything's possible if you've got enough nerve.",source:"-Ginny Weasley", citation:"Harry Potter and the Order of the Phoenix"},
+  {quote:"I love magic.", source:"-Harry Potter", citation:" Harry Potter and the Goblet of Fire"},
+  {quote:"Never trust anything that can think for itself if you can't see where it keeps its brain. ", source:"-Arthur Weasley", citation:"Harry Potter and the Chamber of Secrets", chapter:"Chapter 18"},
+  {quote:"Happiness can be found even in the darkest of times, if one only remembers to turn on the light.", source:"-Albus Dumbledore",citation:"Harry Potter and the Prisoner of Azkaban"},
 ];
 /***
  * `getRandomQuote` function
@@ -85,10 +88,11 @@ function printQuote() {
 document.getElementById("quote-box").innerHTML = html;
 document.body.style.background = storeColors.houseColors;
 }
-/**document.body links the background to the index.html.and the style.css togethre.**/
+/**document.body links the background to the index.html.and the style.css togethre. 
+ * i found the code at w#schools.com/jsref/prop_style_backgroundcolor.asp **/
 /**document.getElementById links innerHTML and the html string to index.html 
  * and displays it ong the site.
- * = html is my code to the function printQuote */ 
+ * = html is my code to the function printQuote (You add quote + source + citation + chapter) */ 
 /***
  * click event listener for the print quote button
  * DO NOT CHANGE THE CODE BELOW!!
